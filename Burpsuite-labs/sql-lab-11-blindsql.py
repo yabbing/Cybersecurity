68fa8a56824f855d4a373d4c6063eb7d742e7d04
yabbing <sabbatino99@gmail.com>
2026-01-05 17:26:13 -0500

    moving computers

--- Burpsuite-labs/sql-lab-11-blindsql.py
+++ Burpsuite-labs/sql-lab-11-blindsql.py
@@ -6,10 +6,19 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http: 127.0.0.1:8080', 'https: 127.0.0.1:8080'}
 
+def sqki_password(url):
+   password = ""
+   for i in range(1, 21):
+      for j in range (32,126):
+
 def main():
    if len(sys.argv) != 2:
-      print("(+)" Usage: %s <url>" % sys.argv[0])
+      print("(+) Usage: %s <url>" % sys.argv[0])
       print("(+ )Example: %s http://target.com/vulnerable_endpoint" % sys.argv[0])
+   url = sys.argv[1]
+   print("(+) Retrieving administrator password...")
+   sqli_password(url)
+
 
       If __name__ == "__main__":
         main()
